473f08789d8eae609dbb8939894aec5065743d92
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2018-04-15 17:42:21 +0700

    simplify script and add comments

--- lunar-eclipse-prediction.py
+++ lunar-eclipse-prediction.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python
+'''
+lunar-eclipse-prediction.py
+
+Shows the occurences of a lunar eclipse in the 21st century.
+Works by iterating every hour in the 21st century and calculating if the
+separation between the Moon and the Sun is less than 0.9° from 180°.
+The number 0.9° is hardcoded for simplicity, for more accuracy, it
+should be computed from the distance of the Moon and the Sun.
+'''
 import ephem
 from datetime import datetime, timedelta
 
-starttime = datetime(2018, 1, 1, 0, 0, 0)
+curtime = datetime(2001, 1, 1, 0, 0, 0)
 endtime = datetime(2100, 12, 31, 23, 59, 59)
-resdelta = timedelta(seconds = 3600)
-skipdelta = timedelta(hours = 24)
-
-curtime = starttime
 moon = ephem.Moon()
 sun = ephem.Sun()
+observer = ephem.Observer()
+observer.elevation = -6371000
+observer.pressure = 0
 
 while curtime <= endtime:
-    curtimes = curtime.strftime('%Y/%m/%d %H:%M:%S')
-
-    observer = ephem.Observer()
-    observer.lon = 0
-    observer.lat = 0
-    observer.elevation = -6371000
-    observer.date = curtimes
-    observer.temp = -272
-    observer.pressure = 0
+    observer.date = curtime.strftime('%Y/%m/%d %H:%M:%S')
 
     moon.compute(observer)
     sun.compute(observer)
-    sep = abs((float(ephem.separation(moon, sun)) / 0.01745329252) - 180)
+    sep = abs((float(ephem.separation(moon, sun))
+        / 0.01745329252) - 180)
 
     if sep < 0.9:
-        print(curtimes, sep)
-        curtime += skipdelta
+        print(curtime.strftime('%Y/%m/%d %H:%M:%S'), sep)
+        curtime += timedelta(days = 1)
     else:
-        curtime += resdelta
+        curtime += timedelta(hours = 1)
